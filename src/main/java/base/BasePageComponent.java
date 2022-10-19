e39fa0604a76877a195a1d41a12c252a1e9c1305
Alexander Borovko <alexvonbor@gmail.com>
2022-10-19 09:10:04 +0300

    fix

--- src/main/java/base/BasePageComponent.java
+++ src/main/java/base/BasePageComponent.java
@@ -3,7 +3,7 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePageComponent {
+public abstract class BasePageComponent {
     protected WebDriver driver;
 
     public BasePageComponent(WebDriver driver){
